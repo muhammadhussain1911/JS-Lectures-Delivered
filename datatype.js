8c9f94545e04538725400308c08782e56ca396d2
Muhammad Hussain <ayesq0003@gmail.com>
2025-11-18 19:23:06 +0500

    Datatype Lecture Delivered

---
+++ datatype.js
@@ -0,0 +1,15 @@
+const myName = "Muhammad Hussain";
+const age = "12";
+const bool = true;
+const noData = undefined;
+const zero = null;
+const sym = Symbol(12);
+const bigNumber= 1234;
+// const sym1 = Symbol(12);
+// console.log(sym===sym1);
+// console.log(typeof age);
+// const myAge = Number(age);
+// console.log(typeof myAge);
+const myNumName = Number(myName);
+console.log(myNumName);
+
