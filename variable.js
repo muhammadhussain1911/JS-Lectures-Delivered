b3a32a840bc88ccff61861fb3680bce3e95c2aa4
Muhammad Hussain <ayesq0003@gmail.com>
2025-11-18 19:02:58 +0500

    variable lecture completed

--- variable.js
+++ variable.js
@@ -1,1 +1,10 @@
-const myName = "Muhammad Hussain"
+// var myName = "Hussain";
+// var myName = "Shahzad";
+// console.log(myName);
+
+// let myName = "Hussain";
+// let myName = "Shahzad";
+// console.log(myName);
+
+const myName = "Hussain";
+myName = "Shahzad";
